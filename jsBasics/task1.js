ab9e846b9961aaab5757c0598d0a7c1817194634
Illia <ilakurinnoj@MacBook-Air-Ila.local>
2024-12-12 10:34:52 +0200

    JavaScript primitive data type

---
+++ jsBasics/task1.js
@@ -0,0 +1,49 @@
+// TASK 1
+let string = 'testich'
+let num = 5
+let bool = true
+let nul = null
+let und
+
+console.log(string)
+console.log(num)
+console.log(bool)
+console.log(nul)
+console.log(und)
+
+// TASK 2
+let name1 = 'Frank'
+let name2 = 'Rocky'
+let conc = 'Hello ' + name1 + " and " + name2 + "!"
+
+console.log(conc)
+
+let greeting = `Hello, ${name1} and ${name2}`
+console.log(greeting)
+
+// TASK 3
+let age = 20
+let isAdult = age >= 18
+
+console.log("Age:", age);
+console.log("Is adult:", isAdult)
+
+// TASK 4.1
+let radius = 5
+let circleArea = Math.PI * radius ** 2
+
+console.log(circleArea.toFixed(2))
+
+// TASK 4.2
+let length = 10.3
+let width = 34.8
+let lenWid = length * width
+
+console.log(lenWid.toFixed(2))
+
+// TASK 4.3
+let rad = 5
+let height = 12
+let volume = Math.PI * Math.pow(rad, 2) * height
+
+console.log(volume.toFixed(2))
